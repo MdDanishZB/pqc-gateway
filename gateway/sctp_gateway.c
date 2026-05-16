fcc94dadfa2864f97f4852c2f9d74bced5af707c
MdDanishZB <mdanishzb13@gmail.com>
2026-05-16 16:07:14 +0530

    multithread_tcp-to-sctp_gateway

--- gateway/sctp_gateway.c
+++ gateway/sctp_gateway.c
@@ -6,11 +6,88 @@
 #include <arpa/inet.h>
 #include <netinet/sctp.h>
 
+#include <pthread.h>
+
 #define TCP_PORT 4000
 #define SCTP_PORT 5000
 
 #define BUFFER_SIZE 1024
 
+void* handle_client(void* arg) {
+
+    int tcp_client_fd = *(int*)arg;
+
+    int sctp_fd;
+
+    struct sockaddr_in sctp_addr;
+
+    char buffer[1024];
+
+    memset(buffer, 0, sizeof(buffer));
+
+    int bytes = recv(tcp_client_fd,
+                     buffer,
+                     sizeof(buffer),
+                     0);
+
+    if(bytes <= 0) {
+
+        close(tcp_client_fd);
+
+        pthread_exit(NULL);
+    }
+
+    buffer[bytes] = '\0';
+
+    printf("\n[Gateway] Received TCP Data: %s\n",
+           buffer);
+
+    sctp_fd = socket(AF_INET,
+                     SOCK_STREAM,
+                     IPPROTO_SCTP);
+
+    if(sctp_fd < 0) {
+
+        perror("SCTP socket failed");
+
+        close(tcp_client_fd);
+
+        pthread_exit(NULL);
+    }
+
+    memset(&sctp_addr, 0, sizeof(sctp_addr));
+
+    sctp_addr.sin_family = AF_INET;
+    sctp_addr.sin_port = htons(5000);
+
+    inet_pton(AF_INET,
+              "127.0.0.1",
+              &sctp_addr.sin_addr);
+
+    if(connect(sctp_fd,
+               (struct sockaddr*)&sctp_addr,
+               sizeof(sctp_addr)) < 0) {
+
+        perror("SCTP connect failed");
+
+        close(sctp_fd);
+        close(tcp_client_fd);
+
+        pthread_exit(NULL);
+    }
+
+    send(sctp_fd,
+         buffer,
+         strlen(buffer),
+         0);
+
+    printf("[Gateway] Forwarded to SCTP Receiver\n");
+
+    close(sctp_fd);
+    close(tcp_client_fd);
+
+    pthread_exit(NULL);
+}
 
 void start_gateway() {
 
@@ -49,58 +126,25 @@ void start_gateway() {
     while(1) {
 
         tcp_client_fd = accept(tcp_server_fd,
-                               NULL,
-                               NULL);
+                            NULL,
+                            NULL);
 
-        printf("TCP Client Connected\n");
+        if(tcp_client_fd < 0) {
 
-        memset(buffer, 0, BUFFER_SIZE);
+            perror("Accept failed");
 
-        int bytes = recv(tcp_client_fd,
-                         buffer,
-                         BUFFER_SIZE,
-                         0);
-
-        if(bytes <= 0) {
-            close(tcp_client_fd);
             continue;
         }
 
-        buffer[bytes] = '\0';
+        printf("\n[Gateway] TCP Client Connected\n");
 
-        printf("Gateway Received TCP Data: %s\n", buffer);
+        pthread_t tid;
 
-        sctp_fd = socket(AF_INET,
-                         SOCK_STREAM,
-                         IPPROTO_SCTP);
+        pthread_create(&tid,
+                    NULL,
+                    handle_client,
+                    &tcp_client_fd);
 
-        memset(&sctp_addr, 0, sizeof(sctp_addr));
-
-        sctp_addr.sin_family = AF_INET;
-        sctp_addr.sin_port = htons(SCTP_PORT);
-
-        inet_pton(AF_INET,
-                  "127.0.0.1",
-                  &sctp_addr.sin_addr);
-
-        if(connect(sctp_fd,
-                   (struct sockaddr*)&sctp_addr,
-                   sizeof(sctp_addr)) < 0) {
-
-            perror("SCTP connect failed");
-
-            close(tcp_client_fd);
-            continue;
-        }
-
-        send(sctp_fd,
-             buffer,
-             strlen(buffer),
-             0);
-
-        printf("Forwarded to SCTP Receiver\n");
-
-        close(sctp_fd);
-        close(tcp_client_fd);
+        pthread_detach(tid);
     }
 }
